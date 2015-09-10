9da40042d80462c690f530e46531001d5b5c4f2c
theodortruffer <tt@studer-raimann.ch>
2015-09-10 12:27:04 +0200

    updated ilias- / plugin-version

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'subscription';
-$version = '2.5.0';
+$version = '2.6.0';
 $ilias_min_version = '4.4.0';
-$ilias_max_version = '5.0.999';
+$ilias_max_version = '5.1.999';
 $responsible = 'Fabian Schmid, Oskar Truffer - studer + raimann ag';
 $responsible_mail = 'support@studer-raimann.ch';
 ?>
