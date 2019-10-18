3829188127aa0943d265c830f1e9bb717d9a4f82
Fabian Schmid <fs@studer-raimann.ch>
2019-10-18 14:17:07 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'subscription';
-$version = '3.10.1';
+$version = '3.10.2';
 $ilias_min_version = '5.3.0';
 $ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag – Team Core 1';
