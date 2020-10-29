32608375ab8fbec84205e0bbdb29a6dd4772704d
Theodor Truffer <tt@studer-raimann.ch>
2020-10-29 14:47:02 +0100

    v3.10.3

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 $id = 'subscription';
-$version = '3.10.2';
+$version = '3.10.3';
 $ilias_min_version = '5.3.0';
 $ilias_max_version = '5.4.999';
 $responsible = 'studer + raimann ag – Team Core 1';
